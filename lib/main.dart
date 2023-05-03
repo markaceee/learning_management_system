7b7b38db11e3188041ad823489fb3d02889e3e46
Gleronnelyn <128757984+Gleronnelyn@users.noreply.github.com>
2023-05-03 17:08:28 +0800

    Updated code

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,10 @@ class LMSApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'LMS App',
+      debugShowCheckedModeBanner: false,
+      title: 'Learning Management System App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
       home: const Dashboard(),
     );
@@ -22,52 +23,169 @@ class LMSApp extends StatelessWidget {
 
 class Dashboard extends StatelessWidget {
   const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/image/background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Spacer(),
+            Center(
+              child: SizedBox(
+                width: 200.0,
+                height: 50.0,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginPage(),
+                      ),
+                    );
+                  },
+                  child: const Text('Login'),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Center(
+              child: SizedBox(
+                width: 200.0,
+                height: 50.0,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const RegisterPage(),
+                      ),
+                    );
+                  },
+                  child: const Text('Register'),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Center(
+              child: SizedBox(
+                width: 200.0,
+                height: 50.0,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CourseList(),
+                      ),
+                    );
+                  },
+                  child: const Text('Courses'),
+                ),
+              ),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: const Center(
+          child: Text('Learning Management System App'),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String? _accountTitle = 'User account';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('LMS App'),
+        title: _accountTitle != null
+            ? Text(
+          _accountTitle!,
+          textAlign: TextAlign.center,
+        )
+            : const Text(
+          'User account',
+          textAlign: TextAlign.center,
+        ),
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Welcome to LMS App',
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const LoginPage(),
-                  ),
-                );
-              },
-              child: const Text('Login'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const RegisterPage(),
-                  ),
-                );
-              },
-              child: const Text('Register'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CourseList(),
-                  ),
-                );
-              },
-              child: const Text('Courses'),
-            ),
+            if (_accountTitle != null) ...[
+              const SizedBox(height: 20),
+              Text(
+                _accountTitle!,
+                style: const TextStyle(fontSize: 30),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const InstructorPage(),
+                      ),
+                    );
+                  },
+                  child: const Text('Instructor'),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: 200,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const StudentPage(),
+                      ),
+                    );
+                  },
+                  child: const Text('Student'),
+                ),
+              ),
+            ],
           ],
         ),
       ),
@@ -75,39 +193,45 @@ class Dashboard extends StatelessWidget {
   }
 }
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+
+
+
+
+
+
+
+class InstructorPage extends StatelessWidget {
+  const InstructorPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: const Text('Instructor Page'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const TextField(
-              decoration: InputDecoration(
-                hintText: 'Email',
-              ),
-            ),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: 'Password',
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Login'),
-            ),
-          ],
-        ),
+      body: const Center(
+        child: Text('This is the instructor page.'),
       ),
     );
   }
 }
+
+class StudentPage extends StatelessWidget {
+  const StudentPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Student Page'),
+      ),
+      body: const Center(
+        child: Text('This is the student page.'),
+      ),
+    );
+  }
+}
+
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -115,7 +239,11 @@ class RegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register'),
+        title: const Text(
+          'Sign Up',
+          textAlign: TextAlign.center,
+        ),
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
@@ -123,23 +251,42 @@ class RegisterPage extends StatelessWidget {
           children: <Widget>[
             const TextField(
               decoration: InputDecoration(
-                hintText: 'Name',
+                hintText: 'First Name...',
               ),
             ),
             const TextField(
               decoration: InputDecoration(
-                hintText: 'Email',
+                hintText: 'Last Name...',
+              ),
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'Email...',
               ),
             ),
             const TextField(
               obscureText: true,
               decoration: InputDecoration(
-                hintText: 'Password',
+                hintText: 'Password...',
               ),
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Register'),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Confirm password...',
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: 200,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  'Sign Up',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
             ),
           ],
         ),
@@ -147,6 +294,8 @@ class RegisterPage extends StatelessWidget {
     );
   }
 }
+
+
 
 class CourseList extends StatelessWidget {
   final List<String> courses = ['Mathematics', 'Science', 'English', 'History', 'Art', 'Music'];
@@ -157,27 +306,36 @@ class CourseList extends StatelessWidget {
     return Scaffold(
       key: UniqueKey(),
       appBar: AppBar(
-        title: const Text('Courses'),
+        title: const Center(
+          child: Text('Courses'),
+        ),
       ),
-      body: ListView.builder(
-        itemCount: courses.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(courses[index]),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => CourseDetails(courseName: courses[index]),
+      body: Center(
+        child: ListView.builder(
+          itemCount: courses.length,
+          itemBuilder: (context, index) {
+            return Center(
+              child: ListTile(
+                title: Center(
+                  child: Text(courses[index]),
                 ),
-              );
-            },
-          );
-        },
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CourseDetails(courseName: courses[index]),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
 }
+
 
 
 class CourseDetails extends StatelessWidget {
@@ -200,20 +358,20 @@ class CourseDetails extends StatelessWidget {
           children: <Widget>[
             Text(
               courseName,
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 24.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 16.0),
-            Text(
+            const SizedBox(height: 16.0),
+            const Text(
               'Course Description:',
               style: TextStyle(
                 fontSize: 18.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 8.0),
+            const SizedBox(height: 8.0),
             Text(
               'This is the description for the $courseName course. '
                   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
@@ -222,25 +380,25 @@ class CourseDetails extends StatelessWidget {
                   'Sed eu velit eros. Aliquam non nunc non sapien feugiat posuere in sed nisi. '
                   'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. '
                   'Nunc varius justo leo, vitae vestibulum ipsum pellentesque at.',
-              style: TextStyle(
+              style: const TextStyle(
                 fontSize: 16.0,
               ),
             ),
-            SizedBox(height: 16.0),
-            Text(
+            const SizedBox(height: 16.0),
+            const Text(
               'Course Materials:',
               style: TextStyle(
                 fontSize: 18.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 8.0),
+            const SizedBox(height: 8.0),
             Expanded(
               child: ListView.builder(
                 itemCount: 10,
                 itemBuilder: (context, index) {
                   return ListTile(
-                    leading: Icon(Icons.file_copy),
+                    leading: const Icon(Icons.file_copy),
                     title: Text('Material ${index + 1}'),
                     onTap: () {
                       // Open file viewer
