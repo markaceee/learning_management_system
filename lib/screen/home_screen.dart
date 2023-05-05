9d2548c5d3ebafb0f6840bd7168494187a9c6f33
Gleronnelyn <128757984+Gleronnelyn@users.noreply.github.com>
2023-05-05 10:02:44 +0800

    Update home screen dart

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -12,8 +12,6 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static List<Widget> _widgetOptions = <Widget>[
     HomePage(),
     Courses(),
